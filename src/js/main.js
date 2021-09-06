ba1294635cca12248f28240694121702b8cac3b1
kuyt1992 <kuyt1992@gmail.com>
2021-09-06 21:33:00 +0900

    feat: 完了機能、削除機能を追加

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,14 @@ var app = new Vue({
     addItem: function(e) {
       if(this.newItem == '') return
       var todo = {
-        item: this.newItem
+        item: this.newItem,
+        isDone: false
       }
       this.todos.push(todo)
       this.newItem = ''
+    },
+    deleteItem: function(index) {
+      this.todos.splice(index, 1)
     }
   }
 })
